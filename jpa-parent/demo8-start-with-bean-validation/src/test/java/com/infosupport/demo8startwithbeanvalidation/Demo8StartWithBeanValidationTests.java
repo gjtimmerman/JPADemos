b220f8f9f99d0d13c9562b82a06422a6986d078c
Gert Jan Timmerman <gertt@infosupport.com>
2022-07-11 09:40:09 +0200

    Added Tests for Bean Validation

--- jpa-parent/demo8-start-with-bean-validation/src/test/java/com/infosupport/demo8startwithbeanvalidation/Demo8StartWithBeanValidationTests.java
+++ jpa-parent/demo8-start-with-bean-validation/src/test/java/com/infosupport/demo8startwithbeanvalidation/Demo8StartWithBeanValidationTests.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
 import java.time.LocalDate;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalUnit;
+import java.util.List;
 import java.util.function.Consumer;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class Demo8StartWithBeanValidationTests {
     private static final Logger logger = LoggerFactory.getLogger(Demo8StartWithBeanValidationTests.class);
@@ -48,5 +53,59 @@ public class Demo8StartWithBeanValidationTests {
         {
             em.persist(p);
         });
+        Person p2 = em.find(Person.class,1);
+        assertThat(p2.getFirstname()).isEqualTo("Jan");
+        assertThat(p2.getLastname()).isEqualTo("Jansen");
+        assertThat(p2.getBirthdate()).isEqualTo(LocalDate.of(2020,10,12));
+        assertThat(p2.getNumberOfChildren()).isEqualTo(3);
     }
+    @Test
+    void createPersonWithInvalidFirstName()
+    {
+        Person p = new Person("","Jansen",LocalDate.of(1990,2,14),0);
+        assertThatThrownBy(()->
+        {
+            executeInTransaction(em ->
+            {
+                em.persist(p);
+            });
+        }).hasCause(new RollbackException("Error while committing the transaction"));
+    }
+    @Test
+    void createPersonWithInvalidLastName()
+    {
+        Person p = new Person("Jan","",LocalDate.of(1990,2,14),0);
+        assertThatThrownBy(()->
+        {
+            executeInTransaction(em ->
+            {
+                em.persist(p);
+            });
+        }).hasCause(new RollbackException("Error while committing the transaction"));
+    }
+    @Test
+    void createPersonWithInvalidBirthDate()
+    {
+        Person p = new Person("Jan","Jansen",LocalDate.now().plusYears(5),0);
+        assertThatThrownBy(()->
+        {
+            executeInTransaction(em ->
+            {
+                em.persist(p);
+            });
+        }).hasCause(new RollbackException("Error while committing the transaction"));
+    }
+    @Test
+    void createPersonWithInvalidNumberOfChildren()
+    {
+        Person p = new Person("","Jansen",LocalDate.of(1990,2,14),200);
+        assertThatThrownBy(()->
+        {
+            executeInTransaction(em ->
+            {
+                em.persist(p);
+            });
+        }).hasCause(new RollbackException("Error while committing the transaction"));
+    }
+
 }
